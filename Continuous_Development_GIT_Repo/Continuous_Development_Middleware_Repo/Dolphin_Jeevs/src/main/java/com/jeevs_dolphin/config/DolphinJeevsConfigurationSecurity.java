12a6219ee98a23545257bc5cddb6210ad4cdd0c7
RanganaJeevanKumar <kumarjeevan690@gmail.com>
2023-05-29 01:23:52 +0530

    To test

--- Continuous_Development_GIT_Repo/Continuous_Development_Middleware_Repo/Dolphin_Jeevs/src/main/java/com/jeevs_dolphin/config/DolphinJeevsConfigurationSecurity.java
+++ Continuous_Development_GIT_Repo/Continuous_Development_Middleware_Repo/Dolphin_Jeevs/src/main/java/com/jeevs_dolphin/config/DolphinJeevsConfigurationSecurity.java
@@ -1,10 +1,8 @@
 package com.jeevs_dolphin.config;
 
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
-import org.springframework.security.config.annotation.web.WebSecurityConfigurer;
 
 public class DolphinJeevsConfigurationSecurity extends SecurityAutoConfiguration {
-	
 	
 
 }
